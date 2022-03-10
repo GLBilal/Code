a7cc85622841ba9b77f83cab46d8fd2e65bd5884
Gilbert Bilal <Gbilal@dev-10.com>
2022-03-10 09:53:35 -0600

    / TODO: implement method to check if a number is in range of another number

--- DemoLoopsAndMethods/Program.cs
+++ DemoLoopsAndMethods/Program.cs
@@ -2,9 +2,14 @@
 {
     class Demo
     {
-        static void Main(String[] args)
+        static void Main()
         {
-
+            // TODO: implement method to check if a number is in range of another number
+        }
+        // access modifier - static (no not) -return type-name(parms in here)
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            return (num - target) <= range;
         }
     }
 }
